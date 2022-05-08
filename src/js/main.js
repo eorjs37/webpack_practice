460d9d04555bba75f8e3f0c33cddf01f83f44f5a
CHOI DAE GEON <chleorjs37@gmail.com>
2022-05-08 11:48:21 +0900

    fourth commit

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import "@babel/polyfill";
 import { pi, power, Foo } from './lib';
 
 console.log(pi);
-console.log('test push 3');
+console.log('test push 4');
 console.log(power(pi, pi));
 
 const f = new Foo();
