c16c4a103977a194435f797e218140fea2850b03
CHOI DAE GEON <chleorjs37@gmail.com>
2022-06-26 21:56:34 +0900

    진행중

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,6 @@
 // src/js/main.js
 // ES6 모듈
 import "@babel/polyfill";
-import { pi, power, Foo } from './lib';
 import "@/assets/img/home.png";
 import "@/assets/img/logo.png";
-
-console.log(pi);
-console.log('test push 4');
-console.log(power(pi, pi));
-
-const f = new Foo();
-console.log(f.foo());
-console.log(f.bar());
+import "@/assets/img/slide-welcome-01.png";
