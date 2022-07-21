4b6652bbafce92b036b6cf5ca31822396fdc1275
maxgunchoi <chleorjs37@gmail.com>
2022-07-21 13:07:54 +0900

    eslint 세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    env: {
-      commonjs: true,
-      node: true,
-      browser: true,
-      es6: true,
-      jest: true,
-    },
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: "module",
-    },
-    extends: [
-      "eslint:recommended",
-      // eslint와 prettier가 충돌나는 부분 비활성화 ( eslint-config-prettier )
-      "prettier",
-    ],
-    plugins: [
-      // prettier 규칙을 eslint에 적용시키게 해줌 ( eslint-plugin-prettier )
-      "prettier",
-      // ES2015의 import/export 구문 지원 ( eslint-plugin-import )
-      "import",
-    ],
-    rules: {
-      // prettier 규칙을 어기면 error 발생
-      "prettier/prettier": ["error"],
-    },
-  };
+  env: {
+    commonjs: true,
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    // eslint와 prettier가 충돌나는 부분 비활성화 ( eslint-config-prettier )
+    "prettier",
+  ],
+  plugins: [
+    // prettier 규칙을 eslint에 적용시키게 해줌 ( eslint-plugin-prettier )
+    "prettier",
+    // ES2015의 import/export 구문 지원 ( eslint-plugin-import )
+    "import",
+  ],
+  rules: {
+    // prettier 규칙을 어기면 error 발생
+    "prettier/prettier": ["error"],
+  },
+};
